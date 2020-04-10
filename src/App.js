143d2088fd441779a7234156fae8c606ea78d5ab
YasiTL <yasaman.most@gmail.com>
2020-04-10 15:06:47 -0500

    is this a top bar?

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import TopBar from './components/TopBar'
+// import Button from '@material-ui/core/Button'
+// import AppBar from '@material-ui/core/AppBar'
+// import Toolbar from '@material-ui/core/IconButton'
+// import IconButton from '@material-ui/core/IconButton'
+// import MenuIcon from '@material-ui/icons/Menu';
+// import Typography from '@material-ui/core/Typography'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TopBar/>
     </div>
   );
 }
