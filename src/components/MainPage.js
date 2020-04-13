c38c52ec5e178a7e996b4a2e22b3c32a0513509e
YasiTL <yasaman.most@gmail.com>
2020-04-13 15:47:44 -0500

    start placing text and developing list items

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Divider from '@material-ui/core/Divider';
+import Skeleton from '@material-ui/lab/Skeleton';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -21,10 +26,34 @@ export default function MainPage() {
     <div className={classes.root}>
       <Grid container spacing={3}>
         <Grid item xs={12}>
-          <Paper className={classes.paper}>xs=12</Paper>
-        </Grid>
+        <Typography variant="h2" align='center' gutterBottom>
+           Welcome to the Idea Shop's Inventory
+         </Typography>
+         </Grid>
+        
+        <Grid item xs={3}></Grid>
+        <Grid item xs={6}>
+        <Typography variant="h6" align='center' gutterBottom>
+           Search for an item or select a category.
+         </Typography>
+         </Grid>
+         <Grid item xs={3}></Grid>
       </Grid>
+      <List component="nav">
+        <ListItem>
+          <Skeleton variant="rect" width={210} height={118} />
+            <Typography variant="h5" align='center' gutterBottom>
+              Wooden Item
+            </Typography>
+            <Typography variant="body" align='center' gutterBottom>
+              Some Text descrption 
+            </Typography>
+        </ListItem>
+        <Divider />
+        <ListItem>
+          Test 2
+        </ListItem>
+      </List>
     </div>
   );
 }
-
