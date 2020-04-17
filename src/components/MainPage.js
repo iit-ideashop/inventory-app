378b01706e48aa9a557b62e8d3593bb265b8228d
YasiTL <yasaman.most@gmail.com>
2020-04-17 14:49:58 -0500

    make reusable list items based on an items array

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import Divider from '@material-ui/core/Divider';
 import Skeleton from '@material-ui/lab/Skeleton';
-import { Chip } from '@material-ui/core';
+import { Chip, ListItemAvatar, ListItemText, ListItemSecondaryAction, Button } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -28,17 +27,27 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const titles = ['Wood', 'Paper', 'Acrylic'];
-const listItems = titles.map((titles) =>
+const items = [ 
+  {title:"Wood Block - Maple", description:"this is a description for maple wood product", price:3.40, unit:"per foot"},
+  {title:"Female/Male 'Extension' Jumper Wires (300mm)", description:"this is a description for wires I guess", price:0.40, unit:"per foot"},
+  {title:"Red Acrylic Sheet (12x12 inches)", description:"this is a description for an acrylic sheet", price:6.00, unit:"per sheet"}];
+const listItems = items.map((item) =>
   <div>
-  <ListItem>
-    <Skeleton variant="rect" width={210} height={118} />
-      <Typography variant="h5" align='center' gutterBottom>
-        {titles}
-      </Typography>
-      <Typography variant="body" align='center' gutterBottom>
-        Some Text descrption 
-      </Typography>
+  <ListItem alignItems="flex-start">
+    <ListItemAvatar>
+      <Skeleton variant="rect" width={210} height={118} />
+    </ListItemAvatar>
+    <ListItemText
+      primary={
+      <Typography variant="h5" gutterBottom>
+        {item.title}
+      </Typography>}
+      secondary={item.description}
+    />
+    <ListItemSecondaryAction flexDirection="column">
+      <p>${item.price} {item.unit}</p>
+      <Button variant="contained" color="secondary">Add to Cart</Button>
+    </ListItemSecondaryAction>
   </ListItem>
   <Divider/>
   </div>
